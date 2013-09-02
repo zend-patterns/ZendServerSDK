573f901bd58b56490e243a58fa8b7c7489c8ffbd
Slavey Karadzhov <slavey.karadzhov@zend.com>
2013-09-02 17:41:09 +0200

    Fixed a typo in the definition of the installApp command.

--- module/Client/config/module.config.php
+++ module/Client/config/module.config.php
@@ -30,6 +30,9 @@ return array (
                                     ),
                                     'arrays' => array (
                                         'userParams',
+                                    ),
+                                    'files' => array (
+                                        'zpk',
                                     )
                                 )
                             ),
