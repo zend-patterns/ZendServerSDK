41d16979879a782e2f99bebce3a37b8ec002e66d
Slavey Karadzhov <slavey.karadzhov@zend.com>
2013-09-24 11:21:07 +0200

    Improved the building of the phar file.

--- bin/build-phar.sh
+++ bin/build-phar.sh
@@ -11,17 +11,22 @@ git clone https://github.com/zendtech/ZendServerSDK.git
 cd ZendServerSDK/
 wget http://getcomposer.org/composer.phar
 php composer.phar install --no-dev
+# Update the library to the git version
+(cd vendor/zenddevops/webapi/; git pull origin dev)
 php bin/create-phar.php 
 
-git commit -a -m "Compiled new phar file."
-if [ $? == 0 ]; then
-	git push origin master
-else 
-	echo "Nothing to commit" 	 
+read -p "Do you want to commit-n-push the newly compiled phar file (Y/n)?" RESULT
+if [ "$RESULT" != "n" ]; then
+	git commit -a -m "Compiled new phar file."
+	if [ $? -eq 0 ]; then
+		git push origin master
+	else 
+		echo "Nothing to commit" 	 
+	fi	
 fi
 
 read -p "Do you want to delete the temp directory(Y/n)?" RESULT
-if [ "$RESULT" eq "Y"]; then
+if [ "$RESULT" != "n" ]; then
 	rm -rf $TMP_DIR	
 fi 
 cd $PWD
